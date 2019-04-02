838b79ed6e3de732dbca91fcff6aa66c63aa6020
Blair Gallant <blair.gallant@forgerock.com>
2019-04-02 16:45:53 -0700

    Update customize-am.sh

--- 6.5/frudevops-blair/am/development/customize-am.sh
+++ 6.5/frudevops-blair/am/development/customize-am.sh
@@ -14,6 +14,6 @@ echo ""
 
 # Copy the web.xml file that is in the same directory as this script to the
 # webapps/openam/WEB-INF directory
-cp /git/config/6.5/frudevops-blair/am/development/web.xml ${CATALINA_HOME}/webapps/openam/WEB-INF
+cp /git/config/6.5/frudevops-blair/am/development/web.xml ${CATALINA_HOME}/webapps/ROOT/WEB-INF
 
 echo "Finished customizing the AM web application"
