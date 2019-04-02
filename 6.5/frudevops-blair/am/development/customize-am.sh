4d773b802c056ea8a6cf0d8047181ebeb9c1017c
Blair Gallant <blair.gallant@forgerock.com>
2019-04-02 16:35:15 -0700

    Upload custom am config files.

---
+++ 6.5/frudevops-blair/am/development/customize-am.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script and a supporting web.xml file should be placed in the
+# configuration repository at the path defined by the config.importPath
+# property in the custom.yaml file.
+
+echo "Customizing the AM web application"
+echo ""
+
+echo "Available environment variables:"
+echo ""
+env
+echo ""
+
+# Copy the web.xml file that is in the same directory as this script to the
+# webapps/openam/WEB-INF directory
+cp /git/config/6.5/frudevops-blair/am/development/web.xml ${CATALINA_HOME}/webapps/openam/WEB-INF
+
+echo "Finished customizing the AM web application"
