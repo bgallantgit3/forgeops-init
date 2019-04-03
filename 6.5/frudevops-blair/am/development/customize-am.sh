fe28079f583ee109d05121d84822f35727a76472
Blair Gallant <blair.gallant@forgerock.com>
2019-04-03 08:09:02 -0700

    Update customize-am.sh script with comments.

--- 6.5/frudevops-blair/am/development/customize-am.sh
+++ 6.5/frudevops-blair/am/development/customize-am.sh
@@ -4,16 +4,21 @@
 # configuration repository at the path defined by the config.importPath
 # property in the custom.yaml file.
 
-echo "Customizing the AM web application"
+echo "Customizing the AM web application for CORS web.xml"
 echo ""
 
 echo "Available environment variables:"
+echo "================================"
 echo ""
 env
 echo ""
 
 # Copy the web.xml file that is in the same directory as this script to the
 # webapps/openam/WEB-INF directory
+echo "Copy web.xml file to ${CATALINA_HOME}/webapps/ROOT/WEB-INF"
+echo "=========================================================="
+echo ""
 cp /git/config/6.5/frudevops-blair/am/development/web.xml ${CATALINA_HOME}/webapps/ROOT/WEB-INF
 
-echo "Finished customizing the AM web application"
+echo "Finished customizing the AM web application for CORS web.xml"
+echo ""
