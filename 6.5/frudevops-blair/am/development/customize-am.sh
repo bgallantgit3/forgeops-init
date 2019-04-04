bf2e9ebff8815d332a76d7ec6dcbb4f33f4e3db7
Blair Gallant <blair.gallant@forgerock.com>
2019-04-04 07:09:23 -0700

    Comment out copy in customize-am.sh

--- 6.5/frudevops-blair/am/development/customize-am.sh
+++ 6.5/frudevops-blair/am/development/customize-am.sh
@@ -15,10 +15,10 @@ echo ""
 
 # Copy the web.xml file that is in the same directory as this script to the
 # webapps/openam/WEB-INF directory
-echo "Copy web.xml file to ${CATALINA_HOME}/webapps/ROOT/WEB-INF"
+#echo "Copy web.xml file to ${CATALINA_HOME}/webapps/ROOT/WEB-INF"
 echo "=========================================================="
 echo ""
-cp /git/config/6.5/frudevops-blair/am/development/web.xml ${CATALINA_HOME}/webapps/ROOT/WEB-INF
+#cp /git/config/6.5/frudevops-blair/am/development/web.xml ${CATALINA_HOME}/webapps/ROOT/WEB-INF
 
 echo "Finished customizing the AM web application for CORS web.xml"
 echo ""
