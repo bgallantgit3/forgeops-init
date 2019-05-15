0acbca4179ef7abac31a463f707bb34f3fbf7e72
Blair Gallant <blair.gallant@forgerock.com>
2019-05-15 09:42:20 -0700

    Update config based on sync-with-ldap sample.

--- 6.5/idm400b1/idm/script/owner.js
+++
@@ -1,8 +0,0 @@
-if (source !== "unassigned") {
-  var ownerQry = openidm.query("managed/user", {
-    "_queryFilter": "userName eq '" + source + "'"
-  });
-  if (ownerQry.result.length === 1) {
-    ({ _ref : "managed/user/" + ownerQry.result[0]._id});
-  }
-}
